36ae37a6120545f83c54a90a13806276f71e3280
OndrejSpok <ondrej.spok@gmail.com>
2022-04-21 16:35:48 +0200

    PLease work

---
+++ part1/part1/src/App.js
@@ -0,0 +1,54 @@
+const Header = (props) => {
+  console.log(props)
+  return(
+  <h1>{props.course}
+    </h1>
+  )
+}
+
+const Part = (props) => {
+  console.log(props)
+  return(
+  <p>{props.part} {props.excercise}</p>
+  )
+}
+
+/*Refactor the Content component so that it does not render any names of parts or their number of exercises by itself. 
+Instead it only renders three Part components of which each renders the name and number of exercises of one part. */
+const Content = (props) => {
+  console.log(props)
+  return(
+    <div>
+    <Part part={props.part1} excercise={props.excercise1} />
+    <Part part={props.part2} excercise={props.excercise2} />
+    <Part part={props.part3} excercise={props.excercise3} />
+    </div>
+  )  
+}
+
+const Total = (props) => {
+  console.log(props)
+  return(
+    <p>Number of excercises {props.count}</p>
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header course={course}/>
+      <Content part1={part1} excercise1={exercises1} part2={part2} excercise2={exercises2} part3={part3} excercise3={exercises3} />        
+      <Total count={exercises1 + exercises2 + exercises3} />
+    </div>
+  )
+}
+
+export default App
