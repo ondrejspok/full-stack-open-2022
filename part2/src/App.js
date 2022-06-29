abbc1f8a3a0ad569940e46d63a812859c42eb92a
ondrejspok <ondrej.spok@gmail.com>
2022-06-29 19:10:54 +0200

    ex 2.1 completed

---
+++ part2/src/App.js
@@ -0,0 +1,52 @@
+
+const Header = ({ course }) => <h1>{course}</h1>
+
+const Total = ({ sum }) => <p>Number of exercises {sum}</p>
+
+const Part = ({ part }) => 
+  <p>
+    {part.name} {part.exercises}
+  </p>
+
+const Course = (props) => {
+  console.log(props)
+
+  const parts = props.course.parts.map(course =>
+    <li key={course.id}>{course.name} {course.exercises}</li>
+  )
+
+  return (
+    <div>
+     <Header course={props.course.name}/> 
+     {parts}
+    </div>
+  );
+};
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      },
+    ]
+  }
+
+  return <Course course={course} />
+}
+
+export default App
